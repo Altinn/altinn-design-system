006fd0763d0c553748909c2d13fe1354a0ff3eff
Håkon <2082481+haakemon@users.noreply.github.com>
2022-05-02 14:13:26 +0200

    fix release issues, add a11y check (#2)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
     'storybook-addon-turbo-build',
     'storybook-dark-mode',
   ],

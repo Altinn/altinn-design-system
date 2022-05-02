cd8f96b2a8689bf1f1ac6beea187ed0dcadda9f0
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-05-02 16:12:38 +0200

    fix: incorrect import of design tokens

--- src/DesignTokens/index.ts
+++ src/DesignTokens/index.ts
@@ -1,4 +1,4 @@
-import tokens from '@altinn/figma-design-tokens';
+import * as tokens from '@altinn/figma-design-tokens';
 import jsonTokens from '@altinn/figma-design-tokens/dist/tokens.json';
 
 export { tokens, jsonTokens };

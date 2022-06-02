4917b3251d22d3d76f49f88983aff059130b83a6
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-06-02 14:29:40 +0200

    Toggle button group tests first iteration

--- src/components/ToggleButtonGroup/ToggleButton.test.tsx
+++ src/components/ToggleButtonGroup/ToggleButton.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { render  } from '@testing-library/react';
+import { tokens } from '@/DesignTokens';
 
 import { ToggleButton } from './ToggleButton';
 
 describe('ToggleButton', () => {
-  it('should verify something', () => {
+  it('calls onClick when clicked', () => {
+    // Please add some non-snapshot test to verify conditional statements
+    expect(<ToggleButton />).toBe(true);
+  });
+
+  it('Button has aria pressed state after being clicked', () => {
     // Please add some non-snapshot test to verify conditional statements
     expect(<ToggleButton />).toBe(true);
   });
--- src/components/ToggleButtonGroup/ToggleButtonGroup.test.tsx
+++ src/components/ToggleButtonGroup/ToggleButtonGroup.test.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { ToggleButtonGroup } from './ToggleButtonGroup';
+import { ChangeProps, ToggleButtonGroup } from './ToggleButtonGroup';
+import { ToggleButton } from './ToggleButton';
+
+const [selected, setSelected] = useState('left');
+
+const handleChange = ({ selectedValue }: ChangeProps) => {
+  setSelected(selectedValue);
+};
+
+interface ToggleButtonGroupComponentProps {
+  handleOnChange: ({ selectedValue }: ChangeProps) => string;
+  selectedValue: string;
+}
+
+interface ChangeProps {
+  selectedValue: string;
+}
+
+const ToggleButtonGroupComponent = ({
+  handleOnChange,
+  selectedValue,
+}: ToggleButtonGroupComponentProps) => (
+  <ToggleButtonGroup onChange={handleOnChange} selectedValue={selectedValue}>
+    <ToggleButton value='left' />
+    <ToggleButton value='right' />
+  </ToggleButtonGroup>
+)
 
 describe('ToggleButtonGroup', () => {
-  it('should verify something', () => {
+  it('calls onChange when clicked', () => {
+    <ToggleButtonGroupComponent
+      handleOnChange={handleChange}
+      selected={selected}
+    ></ToggleButtonGroupComponent>;
+  });
+
+  it('Button has aria pressed state after being clicked', () => {
+    // Please add some non-snapshot test to verify conditional statements
+    expect(<ToggleButton />).toBe(true);
+  });
+
+  it('Right button not pressed when left is pressed', () => {
     // Please add some non-snapshot test to verify conditional statements
     expect(<ToggleButtonGroup />).toBe(true);
   });

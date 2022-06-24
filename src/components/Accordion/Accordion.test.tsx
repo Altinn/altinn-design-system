89ad1c21ad82ed1dd8fb277f9a6bf81344ca924b
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-06-24 12:33:59 +0200

    Added tests

--- src/components/Accordion/Accordion.test.tsx
+++ src/components/Accordion/Accordion.test.tsx
@@ -15,7 +15,7 @@ const render = (props: Partial<AccordionProps> = {}) => {
         <AccordionContent>AccordionContent</AccordionContent>
       </>
     ),
-    onChange: jest.fn(),
+    onClick: jest.fn(),
     open: true,
     ...props,
   };
@@ -25,44 +25,75 @@ const render = (props: Partial<AccordionProps> = {}) => {
 const user = userEvent.setup();
 
 describe('Accordion', () => {
-  it('should call handleChange with correct open=true when AccordionHeader is clicked', async () => {
-    const handleChange = jest.fn();
-    render({ onChange: handleChange });
+  it('should call handleClick with open=true when AccordionHeader is clicked', async () => {
+    const handleClick = jest.fn();
+    render({ onClick: handleClick, open: true });
+
     await user.click(screen.getByRole('button', { name: 'AccordionHeader' }));
-    expect(handleChange).toHaveBeenCalledWith({ open: true });
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
 
 describe('Accordion', () => {
-  it('should call handleChange with open=false when AccordionHeader is clicked', async () => {
-    const handleChange = jest.fn();
-    render({ onChange: handleChange });
+  it('should call handleClick with open=false when AccordionHeader is clicked', async () => {
+    const handleClick = jest.fn();
+    render({ onClick: handleClick, open: false });
 
     await user.click(screen.getByRole('button', { name: 'AccordionHeader' }));
-    expect(handleChange).toHaveBeenCalledWith({ open: true });
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
 
 describe('Accordion', () => {
   it('should have aria-expanded=true when clicked and open=true', async () => {
-    const handleChange = jest.fn();
-    render({ onChange: handleChange, open: true });
+    const handleClick = jest.fn();
+    render({ onClick: handleClick });
 
     await user.click(screen.getByRole('button', { name: 'AccordionHeader' }));
     expect(
-      screen.getByRole('button', { name: 'Left', pressed: true }),
+      screen.getByRole('button', { name: 'AccordionHeader', expanded: true }),
     ).toBeInTheDocument();
   });
 });
 
 describe('Accordion', () => {
   it('should have aria-expanded=false when clicked and open=false', async () => {
-    const handleChange = jest.fn();
-    render({ onChange: handleChange, open: false });
+    const handleClick = jest.fn();
+    render({ onClick: handleClick, open: false });
 
     await user.click(screen.getByRole('button', { name: 'AccordionHeader' }));
     expect(
-      screen.getByRole('button', { name: 'Left', pressed: false }),
+      screen.getByRole('button', { name: 'AccordionHeader', expanded: false }),
+    ).toBeInTheDocument();
+  });
+});
+
+describe('Accordion', () => {
+  it('should have aria-expanded=true when accordion is expanded using key press ', async () => {
+    const handleClick = jest.fn();
+    render({ onClick: handleClick });
+
+    const AccordionHeader = screen.getByRole('button', {
+      name: 'AccordionHeader',
+    });
+    await user.type(AccordionHeader, '{Space}');
+    expect(
+      screen.getByRole('button', { name: 'AccordionHeader', expanded: true }),
+    ).toBeInTheDocument();
+  });
+});
+
+describe('Accordion', () => {
+  it('should have aria-expanded=true when accordion is expanded using key press ', async () => {
+    const handleClick = jest.fn();
+    render({ onClick: handleClick, open: false });
+
+    const AccordionHeader = screen.getByRole('button', {
+      name: 'AccordionHeader',
+    });
+    await user.type(AccordionHeader, '{Space}');
+    expect(
+      screen.getByRole('button', { name: 'AccordionHeader', expanded: false }),
     ).toBeInTheDocument();
   });
 });

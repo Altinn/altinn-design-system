b81d1f3f4c9e72e42e2d0ac53d8f101497ad1482
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-08-04 14:15:58 +0200

    Fix context

--- src/components/List/Context.ts
+++ src/components/List/Context.ts
@@ -1,18 +1,15 @@
 import { createContext, useContext } from 'react';
 
-import type { BorderStyle } from './List';
+import { BorderStyle } from './List';
 
-export const ListContext = createContext<
-  | {
-      borderStyle: BorderStyle;
-    }
-  | undefined
->(undefined);
+export const ListContext = createContext({
+  borderStyle: BorderStyle.Solid,
+});
 
 export const useListContext = () => {
   const context = useContext(ListContext);
   if (context === undefined) {
-    throw new Error('useListContext must be used within an useListContext');
+    throw new Error('useListContext must be used within a ListContext');
   }
 
   return context;

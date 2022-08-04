a1d5598ff63efa58badabc961223dc73d6716f80
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2022-08-04 15:06:24 +0200

    Feat/dashed list border style (#74)

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 
 import classes from './ListItem.module.css';
-
+import { useListContext } from './Context';
 export interface ListItemProps {
   children?: React.ReactNode;
 }
 
 export const ListItem = ({ children }: ListItemProps) => {
-  return <li className={classes['list-item']}>{children}</li>;
+  const { borderStyle } = useListContext();
+  return (
+    <li
+      className={cn(classes['list-item'], classes[`list-item--${borderStyle}`])}
+    >
+      {children}
+    </li>
+  );
 };

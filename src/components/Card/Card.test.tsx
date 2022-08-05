ebb07a8e6aeee05eebafc33c2695aa9072766e21
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-08-05 09:06:43 +0200

    Made tests

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render as renderRtl } from '@testing-library/react';
+import { render as renderRtl, screen } from '@testing-library/react';
 
 import type { CardProps } from './Card';
 import { Card } from './Card';
 import { CardHeader } from './CardHeader';
 import { CardContent } from './CardContent';
+import { CardVariant } from './Context';
 
 const render = (props: Partial<CardProps> = {}) => {
   const allProps = {
@@ -22,6 +23,55 @@ const render = (props: Partial<CardProps> = {}) => {
 };
 
 describe('Card', () => {
-  it('Should be variant primary when variant is not defined');
-  render();
+  Object.values(CardVariant).forEach((variant) => {
+    it(`should render a CardHeader and CardContent with ${variant} classname when variant is ${variant}`, () => {
+      render({ variant });
+      const otherVariants = Object.values(CardVariant).filter(
+        (v) => v !== variant,
+      );
+
+      const cardHeader = screen.getByTestId('card-header');
+      const cardContent = screen.getByTestId('card-content');
+
+      expect(cardHeader.classList.contains(`card-header--${variant}`)).toBe(
+        true,
+      );
+      otherVariants.forEach((v) => {
+        expect(cardHeader.classList.contains(`card-header--${v}`)).toBe(false);
+      });
+      expect(cardContent.classList.contains(`card-content--${variant}`)).toBe(
+        true,
+      );
+      otherVariants.forEach((v) => {
+        expect(cardContent.classList.contains(`card-content--${v}`)).toBe(
+          false,
+        );
+      });
+    });
+
+    it(`should render a CardHeader and CardContent with ${variant} classname when variant is not set`, () => {
+      render();
+      const otherVariants = Object.values(CardVariant).filter(
+        (v) => v !== variant,
+      );
+
+      const cardHeader = screen.getByTestId('card-header');
+      const cardContent = screen.getByTestId('card-content');
+
+      expect(cardHeader.classList.contains(`card-header--${variant}`)).toBe(
+        true,
+      );
+      otherVariants.forEach((v) => {
+        expect(cardHeader.classList.contains(`card-header--${v}`)).toBe(false);
+      });
+      expect(cardContent.classList.contains(`card-content--${variant}`)).toBe(
+        true,
+      );
+      otherVariants.forEach((v) => {
+        expect(cardContent.classList.contains(`card-content--${v}`)).toBe(
+          false,
+        );
+      });
+    });
+  });
 });

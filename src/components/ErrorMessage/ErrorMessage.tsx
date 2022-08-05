4ef7467b3b234262284e03bfadaa0d41c901f5ee
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2022-08-05 15:19:22 +0200

    add more styling and children prop

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -4,9 +4,20 @@ import cn from 'classnames';
 import classes from './ErrorMessage.module.css';
 
 export interface ErrorMessageProps {
-  message?: string;
+  id?: string;
+  message?: string | React.ReactNode;
+  children?: React.ReactNode;
 }
 
-export const ErrorMessage = ({ message }: ErrorMessageProps) => {
-  return <div className={cn(classes['error-message-wrapper'])}>{message}</div>;
+export const ErrorMessage = ({ id, message, children }: ErrorMessageProps) => {
+  return (
+    <div
+      data-testid={`message-component-${id}`}
+      id={id}
+      key={id}
+      className={cn(classes['error-message-wrapper'])}
+    >
+      {message ? message : children}
+    </div>
+  );
 };

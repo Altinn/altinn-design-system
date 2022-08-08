30bd3dc50cdaa06fa827dd77b474cc47fb64bbfc
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-08-08 14:37:09 +0200

    Test debugging

--- src/components/Popover/Popover.test.tsx
+++ src/components/Popover/Popover.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { render as renderRtl, screen } from '@testing-library/react';
 
-import { Button } from '../Button/Button';
-
 import type { PopoverProps } from './Popover';
 import { Popover } from './Popover';
 import { PopoverTrigger } from './PopoverTrigger';
@@ -15,7 +13,7 @@ const render = (props: Partial<PopoverProps> = {}) => {
     children: (
       <>
         <PopoverTrigger>
-          <div>Popover Trigger</div>
+          <div>trigger</div>
         </PopoverTrigger>
         <PopoverContent>PopoverContent</PopoverContent>
       </>
@@ -29,23 +27,31 @@ const render = (props: Partial<PopoverProps> = {}) => {
 const user = userEvent.setup();
 
 describe('Popover', () => {
-  /*it('should have classname ', async () => {
-    await user.click(screen.getByRole('button', { name: 'PopoverTrigger' }));
+  it('should have classname ', async () => {
+    /*await user.click(screen.getByRole('button', { name: 'PopoverTrigger' }));
     expect(
       screen.getByRole('dialog', { name: 'PopoverContent' }),
-    ).toBeInTheDocument();
-  });*/
+    ).toBeInTheDocument();*/
+    render();
+    await user.click(
+      screen.getByRole('button', {
+        name: /trigger/i,
+      }),
+    );
+    //await screen.debug();
+  });
 
-  Object.values(PopoverVariant).forEach((variant) => {
+  /*Object.values(PopoverVariant).forEach((variant) => {
     it(`should render popover with correct classname when variant is ${variant}`, async () => {
-      render({ variant });
+      render();
       const otherVariants = Object.values(PopoverVariant).filter(
         (v) => v !== variant,
       );
-      await user.click(screen.getByTestId('popover-trigger'));
-      const popoverContent = screen.getByTestId('popover-content');
+      //await user.click(screen.getByTestId('popover-trigger'));
+      screen.debug();
+      /*await user.click(screen.getByTestId('popover-trigger'));
       const popoverArrow = screen.getByTestId('popover-arrow');
-
+      //const popoverContent = screen.getByTestId('popover-content');
       expect(popoverArrow.classList.contains(`popover-arrow--${variant}`)).toBe(
         true,
       );
@@ -53,8 +59,9 @@ describe('Popover', () => {
         expect(popoverArrow.classList.contains(`popover-arrow--${v}`)).toBe(
           false,
         );
-      });
-      expect(
+      });*/
+
+  /*expect(
         popoverContent.classList.contains(`popover-content--${variant}`),
       ).toBe(true);
       otherVariants.forEach((v) => {
@@ -63,5 +70,5 @@ describe('Popover', () => {
         );
       });
     });
-  });
+  });*/
 });

9343368a69e4db1d7fe8aada771a0bd003acfed1
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-08-10 16:55:55 +0200

    fix tests

--- src/components/Popover/Popover.test.tsx
+++ src/components/Popover/Popover.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { render as renderRtl, screen } from '@testing-library/react';
+import { act, render as renderRtl, screen } from '@testing-library/react';
 
 import type { PopoverProps } from './Popover';
 import { Popover } from './Popover';
@@ -8,36 +8,6 @@ import { PopoverTrigger } from './PopoverTrigger';
 import { PopoverContent } from './PopoverContent';
 import { PopoverVariant } from './Context';
 
-/*
-class ResizeObserver {
-  cb: any;
-
-  constructor(cb: any) {
-    this.cb = cb;
-  }
-  observe(cb: any) {
-    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }]);
-  }
-  unobserve() {}
-  disconnect() {}
-}
-
-
-// @ts-ignore
-global.ResizeObserver = ResizeObserver;
-
-global.DOMRect = {
-  // @ts-ignore
-  fromRect: () => ({
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    width: 0,
-    height: 0,
-  }),
-};
-*/
 const render = (props: Partial<PopoverProps> = {}) => {
   const allProps = {
     children: (
@@ -58,12 +28,14 @@ const user = userEvent.setup();
 
 Object.values(PopoverVariant).forEach((variant) => {
   it(`should render popover arrow and popover content with correct classname when variant is ${variant}`, async () => {
-    render({ variant: variant });
+    render({ variant });
     const otherVariants = Object.values(PopoverVariant).filter(
       (v) => v !== variant,
     );
     const popoverTrigger = screen.getByRole('button', { name: 'Trigger' });
-    user.click(popoverTrigger);
+    await act(async () => {
+      await user.click(popoverTrigger);
+    });
     const popoverContent = screen.getByTestId('popover-content');
     const popoverArrow = screen.getByTestId('popover-arrow');
     expect(popoverArrow.classList.contains(`popover-arrow--${variant}`)).toBe(
--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+// @radix-ui/react-popover uses resizeobserver, which is not supported in jsdom.
+// This is a simple mock to not break the tests.
+class ResizeObserver {
+  observe = jest.fn();
+  unobserve = jest.fn();
+  disconnect = jest.fn();
+}
+window.ResizeObserver = ResizeObserver;

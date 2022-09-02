63c9b708afdb7d21f870abdda82da15baf2c0a21
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-09-02 15:04:13 +0200

    fix tests

--- src/components/Panel/PopoverPanel.test.tsx
+++ src/components/Panel/PopoverPanel.test.tsx
@@ -22,29 +22,82 @@ const render = (props: Partial<PopoverPanelProps> = {}) => {
 const user = userEvent.setup();
 
 describe('Panel', () => {
-  it('should call onOpenChange when popover trigger is clicked', async () => {
-    const onOpenChange = jest.fn();
-    render({ onOpenChange });
-    const popoverTrigger = screen.getByRole('button', { name: 'Open' });
+  describe('onOpenChange', () => {
+    it('should call onOpenChange with true when popover trigger is clicked', async () => {
+      const onOpenChange = jest.fn();
+      render({ onOpenChange });
+      const popoverTrigger = screen.getByRole('button', { name: 'Open' });
 
-    await user.click(popoverTrigger);
-    expect(onOpenChange).toHaveBeenCalledWith(true);
-    expect(onOpenChange).toHaveBeenCalledTimes(1);
-
-    await act(async () => {
-      await user.type(popoverTrigger, '{Space}');
+      await user.click(popoverTrigger);
+      expect(onOpenChange).toHaveBeenCalledWith(true);
+      expect(onOpenChange).toHaveBeenCalledTimes(1);
     });
-    expect(onOpenChange).toHaveBeenCalledWith(true);
-    expect(onOpenChange).toHaveBeenCalledTimes(2);
+
+    it('should call onOpenChange with false when trigger is clicked and open is true', async () => {
+      const onOpenChange = jest.fn();
+      await act(async () => {
+        render({ onOpenChange, open: true });
+      });
+      const popoverTrigger = screen.getByRole('button', { name: 'Open' });
+
+      await user.click(popoverTrigger);
+      expect(onOpenChange).toHaveBeenCalledWith(false);
+      expect(onOpenChange).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call onOpenChange with true when trigger is clicked by space', async () => {
+      const onOpenChange = jest.fn();
+      render({ onOpenChange });
+      const popoverTrigger = screen.getByRole('button', { name: 'Open' });
+      popoverTrigger.focus();
+      await user.keyboard('[Space]');
+      expect(onOpenChange).toHaveBeenCalledWith(true);
+      expect(onOpenChange).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call onOpenChange with false when trigger is clicked by space and open is true', async () => {
+      const onOpenChange = jest.fn();
+      await act(async () => {
+        render({ onOpenChange, open: true });
+      });
+      const popoverTrigger = screen.getByRole('button', { name: 'Open' });
+      popoverTrigger.focus();
+      await user.keyboard('[Space]');
+      expect(onOpenChange).toHaveBeenCalledWith(false);
+      expect(onOpenChange).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call onOpenChange with true when trigger is clicked by enter', async () => {
+      const onOpenChange = jest.fn();
+      render({ onOpenChange });
+      const popoverTrigger = screen.getByRole('button', { name: 'Open' });
+      popoverTrigger.focus();
+      await user.keyboard('[Enter]');
+      expect(onOpenChange).toHaveBeenCalledWith(true);
+      expect(onOpenChange).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call onOpenChange with false when trigger is clicked by enter and open is true', async () => {
+      const onOpenChange = jest.fn();
+      await act(async () => {
+        render({ onOpenChange, open: true });
+      });
+      const popoverTrigger = screen.getByRole('button', { name: 'Open' });
+      popoverTrigger.focus();
+      await user.keyboard('[Enter]');
+      expect(onOpenChange).toHaveBeenCalledWith(false);
+      expect(onOpenChange).toHaveBeenCalledTimes(1);
+    });
   });
 
   it('should show popover content when isOpen=true', async () => {
-    render({ open: true });
-
+    await act(async () => {
+      render({ open: true });
+    });
     expect(screen.getByTestId('popover-content')).toBeInTheDocument();
   });
 
-  it('should not show popover content when isOpen=false', async () => {
+  it('should not show popover content when isOpen=false', () => {
     render({ open: false });
 
     expect(screen.queryByTestId('popover-content')).not.toBeInTheDocument();

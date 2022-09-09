8f2e4a96efb65dfa1347125e650bfbd32995f241
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-09-09 14:13:41 +0200

    Export new files

--- src/components/Accordion/index.ts
+++ src/components/Accordion/index.ts
@@ -1,3 +1,5 @@
 export { Accordion } from './Accordion';
 export { AccordionHeader } from './AccordionHeader';
 export { AccordionContent } from './AccordionContent';
+export { AccordionIconVariant } from './Context';
+export { AccordionIcon } from './AccordionIcon';

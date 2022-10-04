7a8c9527a241fc170b11026d8a9390dc0af93c27
Caroline Stensland Selte <cs@itverket.no>
2022-10-04 13:15:36 +0200

    Add pagination component with pagination buttons

---
+++ src/components/Pagination/Pagination.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import { config } from 'storybook-addon-designs';
+
+import { StoryPage } from '@sb/StoryPage';
+
+import { Pagination } from './Pagination';
+
+const figmaLink = ''; // TODO: Add figma link
+
+export default {
+  title: `Components/Pagination`,
+  component: Pagination,
+  parameters: {
+    design: config([
+      {
+        type: 'figma',
+        url: figmaLink,
+      },
+      {
+        type: 'link',
+        url: figmaLink,
+      },
+    ]),
+    docs: {
+      page: () => (
+        <StoryPage
+          description={`TODO: Add a description (supports markdown)`}
+        />
+      ),
+    },
+  },
+  args: {
+    //TODO: Add default args
+  },
+} as ComponentMeta<typeof Pagination>;
+
+const Template: ComponentStory<typeof Pagination> = (args) => (
+  <Pagination {...args} />
+);
+
+export const Example = Template.bind({});
+Example.args = {
+  numberOfPages: 3,
+};
+Example.parameters = {
+  docs: {
+    description: {
+      story: '', // TODO: add story description, supports markdown
+    },
+  },
+};

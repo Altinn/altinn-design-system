a1b9665caa927fca4a7c85c79b957331ac1c7ee3
Caroline Stensland Selte <cs@itverket.no>
2022-10-04 13:15:36 +0200

    Add pagination component with pagination buttons

---
+++ src/components/Pagination/Pagination.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Pagination } from './Pagination';
+
+describe('Pagination', () => {
+  it('should verify something', () => {
+    // Please add some non-snapshot test to verify conditional statements
+    expect(<Pagination />).toBe(true);
+  });
+});

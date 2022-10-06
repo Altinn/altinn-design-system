261cb0fd687baffadc02391194aee61d92da7282
Caroline Stensland Selte <cs@itverket.no>
2022-10-06 08:41:06 +0200

    Add pagination to footer in table stories

--- src/components/Table/TableCell.tsx
+++ src/components/Table/TableCell.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 
 import classes from './TableCell.module.css';
-import { useTableRowTypeContext } from './Context';
+import { useTableRowTypeContext, Variant } from './Context';
 
 export interface TableCellProps {
   children?: React.ReactNode;
@@ -17,25 +17,28 @@ export const TableCell = ({ children, colSpan = 1 }: TableCellProps) => {
   const { variant } = useTableRowTypeContext();
   return (
     <React.Fragment>
-      {variant === 'header' ? (
+      {variant === Variant.Header && (
         <th
           className={cn(classes['header-table-cell'])}
           colSpan={colSpan}
         >
-          {' '}
           <div className={cn(classes['input'])}>{children}</div>
         </th>
-      ) : (
-        variant === 'body' && (
-          <React.Fragment>
-            <td
-              className={cn(classes['body-table-cell'])}
-              colSpan={colSpan}
-            >
-              <div className={cn(classes['input'])}>{children}</div>
-            </td>
-          </React.Fragment>
-        )
+      )}
+      {variant === Variant.Body && (
+        <React.Fragment>
+          <td
+            className={cn(classes['body-table-cell'])}
+            colSpan={colSpan}
+          >
+            <div className={cn(classes['input'])}>{children}</div>
+          </td>
+        </React.Fragment>
+      )}
+      {variant === Variant.Footer && (
+        <td colSpan={colSpan}>
+          <div className={cn(classes['input'])}>{children}</div>
+        </td>
       )}
     </React.Fragment>
   );

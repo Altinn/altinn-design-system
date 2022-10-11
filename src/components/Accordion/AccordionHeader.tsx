035118683cfbc853706fe531c7ef856123f8bd87
Alexandra Vedeler <arnex04@gmail.com>
2022-10-11 08:51:12 +0200

    Fixes to accordion (#145)
    * add vscode to gitignore
    
    * multiple changes and fixes on the accoridon
    
    * conditional rendering of subtitle
    
    * removed fixed input to header end section, hide subtitle when xs screen and added tests for subtitle
    
    * rollback a couple of changes
    
    * rollback another small change
    
    * typo
    
    * requested changes
    
    * remove unnessesary extension
    
    * fixed new tests after changes
    
    * separate out tests for Header

--- src/components/Accordion/AccordionHeader.tsx
+++ src/components/Accordion/AccordionHeader.tsx
@@ -8,16 +8,22 @@ import { AccordionIcon } from './AccordionIcon';
 export interface AccordionHeaderProps {
   children?: React.ReactNode;
   actions?: React.ReactNode;
+  subtitle?: string;
 }
 
 export const AccordionHeader = ({
   children,
   actions,
+  subtitle,
 }: AccordionHeaderProps) => {
   const { onClick, open, headerId, contentId } = useAccordionContext();
 
   return (
-    <div className={cn(classes['accordion-header'])}>
+    <div
+      className={cn(classes['accordion-header'], {
+        [classes['accordion-header--subtitle']]: subtitle,
+      })}
+    >
       <AccordionIcon />
       <button
         className={cn(classes['accordion-header__title'])}
@@ -28,10 +34,16 @@ export const AccordionHeader = ({
         aria-controls={contentId}
       >
         {children}
+        {subtitle?.length && (
+          <div
+            data-testid='accordion-header-subtitle'
+            className={cn(classes['accordion-header__subtitle'])}
+          >
+            {subtitle}
+          </div>
+        )}
       </button>
       <div className={cn(classes['accordion-header__actions'])}>{actions}</div>
     </div>
   );
 };
-
-export default AccordionHeader;

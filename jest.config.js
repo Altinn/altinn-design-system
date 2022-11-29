350b061f3a2b7291599747316d6515d48a184149
Tomas <tomas.engebretsen@gmail.com>
2022-11-29 13:08:28 +0100

    Remove PNG imports

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
   moduleNameMapper: {
     '.(css|less|scss)$': 'identity-obj-proxy',
     '\\.svg$': '<rootDir>/__mocks__/svg.ts',
-    '\\.png$': '<rootDir>/__mocks__/png.ts',
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@test/(.*)$': '<rootDir>/test/$1',
   },

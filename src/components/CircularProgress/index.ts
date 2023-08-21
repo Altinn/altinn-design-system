729f2a5f650b43cd621c94ff2a8121900edba792
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-08-21 15:45:56 +0200

    cleanup

--- src/components/CircularProgress/index.ts
+++ src/components/CircularProgress/index.ts
@@ -1,2 +1,4 @@
-export { CircularProgress } from './CircularProgress';
-export type { CircularProgressProps } from './CircularProgress';
+export {
+  CircularProgress,
+  type CircularProgressProps,
+} from './CircularProgress';
